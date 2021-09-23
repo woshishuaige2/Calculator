14645cf5a0662153750f0337b14d0fd4226ab55e
woshishuaige2 <56005226+woshishuaige2@users.noreply.github.com>
2021-09-22 22:17:46 -0500

    first v

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(){}
+    public void plus(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.textField1);
+        String str1 = myTextField1.getText().toString();
+        EditText myTextField2 = (EditText) findViewById(R.id.textField2);
+        String str2 = myTextField2.getText().toString();
+
+        int a = Integer.parseInt(str1);
+        int b = Integer.parseInt(str2);
+        int c = a+b;
+
+        Intent intent = new Intent (this, answer.class);
+        intent.putExtra("answer", c);
+        startActivity(intent);
+    }
+
+    public void minus(View view) {
+
+    }
+
+    public void divide(View view) {
+    }
+
+    public void multiply(View view) {
+    }
 }
--- app/src/main/java/com/example/calculator/answer.java
+++ app/src/main/java/com/example/calculator/answer.java
@@ -2,13 +2,23 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class answer extends AppCompatActivity {
+
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answer);
+
+        textView2 = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("answer");
+
+        textView2.setText("Hello " + str);
     }
 }
